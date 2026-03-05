fb3a1e8dad2241f809b6ff647657a43e43863c17
Yaroslav_Savchenko <lordik10032018@gmail.com>
2026-03-05 13:43:05 +0300

    uppdate price-modal

--- src/partials/modal-menu.js
+++ src/partials/modal-menu.js
@@ -12,18 +12,38 @@ closeBurgerMenu.addEventListener('click', () => {
   modalBrg.classList.add('is-hidden');
 });
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
+const openBtn1 = document.getElementById('openMenuPrice1');
+const closeBtn1 = document.getElementById('closePriceMenu1');
+const backdrop1 = document.getElementById('backdropPrice1');
+
+closeBtn1.addEventListener('click', () => {
+  backdrop1.classList.remove('is-open');
+});
+
+openBtn1.addEventListener('click', () => {
+  backdrop1.classList.add('is-open');
+});
+
+const openBtn2 = document.getElementById('openMenuPrice2');
+const closeBtn2 = document.getElementById('closePriceMenu2');
+const backdrop2 = document.getElementById('backdropPrice2');
+
+closeBtn2.addEventListener('click', () => {
+  backdrop2.classList.remove('is-open');
+});
+
+openBtn2.addEventListener('click', () => {
+  backdrop2.classList.add('is-open');
+});
+
+const openBtn3 = document.getElementById('openMenuPrice3');
+const closeBtn3 = document.getElementById('closePriceMenu3');
+const backdrop3 = document.getElementById('backdropPrice3');
+
+closeBtn3.addEventListener('click', () => {
+  backdrop3.classList.remove('is-open');
+});
+
+openBtn3.addEventListener('click', () => {
+  backdrop3.classList.add('is-open');
+});
