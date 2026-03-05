5b33c2eecdc71eab295a8faf8cc839499473efa1
Artemenko Pavlo <artemnko67@gmail.com>
2026-03-05 13:18:38 +0200

    added animation btn abd close

--- src/partials/modal-menu.js
+++ src/partials/modal-menu.js
@@ -11,3 +11,19 @@ openBurgerMenu.addEventListener('click', () => {
 closeBurgerMenu.addEventListener('click', () => {
   modalBrg.classList.add('is-hidden');
 });
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
